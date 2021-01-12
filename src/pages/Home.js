992c8b25c3e233e8ca3341a73206fbe77a7dc1e6
Jan Justyn Dela Cruz <blitzersonicz@gmail.com>
2021-01-12 11:13:00 -0500

    Stated adding content in the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,82 @@
 import React, { Component } from "react";
-import { Container, Header, Segment } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import {
+  Container,
+  Header,
+  Segment,
+  Grid,
+  Button,
+  Icon,
+  Image,
+  Card,
+} from "semantic-ui-react";
+import "../styles/Main.scss";
+import calculator from "../styles/assets/calculator.png";
+import todo from "../styles/assets/todo.png";
 
 export class Home extends Component {
   render() {
     return (
-      <Segment textAlign="center" vertical style={{ minHeight: "500" }}>
-        <Container text>
-          <Header as="h1" content="Welcome to my Website!" />
-        </Container>
-      </Segment>
+      <>
+        <Grid>
+          <Grid.Column>
+            <Segment textAlign="center" vertical className="home--hero">
+              <Container text>
+                <Header
+                  as="h1"
+                  content="Welcome to my Website!"
+                  className="home--header"
+                  inverted
+                />
+                <Header
+                  as="h2"
+                  content="This website contains small applications using React.js"
+                  inverted
+                />
+                <Button primary size="huge" as={Link} to="/all-apps">
+                  View All Apps
+                  <Icon name="right arrow" />
+                </Button>
+              </Container>
+            </Segment>
+          </Grid.Column>
+        </Grid>
+        <Segment basic vertical textAlign="center">
+          <Container text>
+            <Header as="h1" content="React Applications" />
+          </Container>
+        </Segment>
+        <Grid container columns={3} doubling stackable>
+          <Grid.Column>
+            <Segment basic>
+              <Card>
+                <Image src={calculator} wrapped ui={false} />
+                <Card.Content>
+                  <Card.Header>Calculator</Card.Header>
+                  <Card.Description>
+                    A basic calcualtor that uses React.js to add, subtract,
+                    divide, and multiply.
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment basic>
+              <Card>
+                <Image src={todo} wrapped ui={false} />
+                <Card.Content>
+                  <Card.Header>Todo</Card.Header>
+                  <Card.Description>
+                    A todo application that enables user to add, edit, and
+                    delete a todo list.
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+            </Segment>
+          </Grid.Column>
+        </Grid>
+      </>
     );
   }
 }
