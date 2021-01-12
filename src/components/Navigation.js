8b6373752baf9c1749fc0bc05f218a1bbce7dff6
Jan Justyn Dela Cruz <blitzersonicz@gmail.com>
2021-01-12 11:11:16 -0500

    Replaced for the React Logo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
-import { Container, Menu, Dropdown } from "semantic-ui-react";
+import { Container, Menu, Dropdown, Image } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import Logo from "../styles/assets/React.png";
+import "../styles/components/navigation.scss";
 
 export class menu extends Component {
   render() {
     return (
       <Menu fixed="top" inverted>
         <Container>
-          <Menu.Item as="a" header>
-            React Apps
+          <Menu.Item header>
+            <Image src={Logo} className="nav--logo" />
           </Menu.Item>
           <Menu.Item as={Link} to="/">
             Home
