462af04979c5c774e3e46a07831b0c399d438990
Jan Justyn Dela Cruz <blitzersonicz@gmail.com>
2021-01-13 09:37:32 -0500

    Added information about the website

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
-import { Container } from "semantic-ui-react";
+import { Container, Header, Segment } from "semantic-ui-react";
 import "../styles/Main.scss";
 
 export class About extends Component {
   render() {
     return (
       <div className="about">
-        <Container>dsdsds</Container>
+        <Container>
+          <Segment vertical textAlign="center">
+            <Header as="h1" content="About this website" />
+          </Segment>
+          <Segment vertical>
+            <p>
+              This website was created for the purpose of showcasing the web
+              applications I have created using React.js. The applications can
+              range from a small to large application, depending on what I am
+              developing. This website is currently using Semantic UI as a CSS
+              framework to style the website and the application.
+            </p>
+          </Segment>
+        </Container>
       </div>
     );
   }
