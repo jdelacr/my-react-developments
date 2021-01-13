8827b640690d4c3692d4fd600d9843469d18b7a3
Jan Justyn Dela Cruz <blitzersonicz@gmail.com>
2021-01-13 09:37:16 -0500

    Added a divider for the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
-import { Container, List, Segment } from "semantic-ui-react";
+import { Container, Divider, List, Segment } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import "../styles/components/footer.scss";
 
 export class Footer extends Component {
   render() {
     return (
-      <Segment vertical>
+      <Segment vertical className="footer">
         <Container textAlign="center">
+          <Divider />
           <List link horizontal divided size="small">
             <List.Item as={Link} to="/">
               Home
